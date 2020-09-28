f13d396a97063c928425828653b1f943cc3937c1
Richard Khims <khims.richard@gmail.com>
2020-09-28 17:33:56 +0400

    refactor handlers

--- internal/currency_service/server/handlers.go
+++ internal/currency_service/server/handlers.go
@@ -1,13 +1,43 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/RichardKhims/go_course/internal/currency_service/database"
+	"github.com/gin-gonic/gin"
+)
 
-func (s *Server) GetClasses(c *gin.Context) {
-	//classes, err := s.db.GetClasses(c.Request.Context())
-	//if err != nil {
-	//	c.Error(err)
-	//}
-	//c.JSON(200, gin.H{
-	//	"result": classes,
-	//})
+func (s *Server) CreateCourse (c *gin.Context) {
+	var input database.Course
+	c.ShouldBindJSON(&input)
+	if err := s.db.CreateCourse(c.Request.Context(), input); err != nil {
+		c.Error(err)
+	} else {
+		c.Status(200)
+	}
+}
+
+func (s *Server) DeleteCourse (c *gin.Context) {
+	var input database.Course
+	c.ShouldBindJSON(&input)
+	if err := s.db.DeleteCourse(c.Request.Context(), input); err != nil {
+		c.Error(err)
+	} else {
+		c.Status(200)
+	}
+}
+
+func (s *Server) ConvertCourse (c *gin.Context) {
+	var input ConvertRequestDTO
+	c.ShouldBindJSON(&input)
+	course, err := s.db.GetCourse(c.Request.Context(), input.CurrencyFrom, input.CurrencyTo)
+	if err != nil {
+		c.Error(err)
+	}
+	if len(course) > 0 {
+		result := input.Value * course[0].Mean
+		c.JSON(200, gin.H{
+			"result": result,
+		})
+	} else {
+		c.String(404, "Pair not found")
+	}
 }
--- internal/currency_service/server/server.go
+++ internal/currency_service/server/server.go
@@ -38,41 +38,7 @@ func (s *Server) Run() {
 func (s *Server) initHandlers() {
 	group := s.router.Group("/api")
 
-	group.POST("/create", func(c *gin.Context) {
-		var input database.Course
-		c.ShouldBindJSON(&input)
-		if err := s.db.CreateCourse(c.Request.Context(), input); err != nil {
-			c.Error(err)
-		} else {
-			c.Status(200)
-		}
-
-	})
-
-	group.DELETE("/delete", func(c *gin.Context) {
-		var input database.Course
-		c.ShouldBindJSON(&input)
-		if err := s.db.DeleteCourse(c.Request.Context(), input); err != nil {
-			c.Error(err)
-		} else {
-			c.Status(200)
-		}
-	})
-
-	group.GET("/convert", func(c *gin.Context) {
-		var input ConvertRequestDTO
-		c.ShouldBindJSON(&input)
-		course, err := s.db.GetCourse(c.Request.Context(), input.CurrencyFrom, input.CurrencyTo)
-		if err != nil {
-			c.Error(err)
-		}
-		if len(course) > 0 {
-			result := input.Value * course[0].Mean
-			c.JSON(200, gin.H{
-				"result": result,
-			})
-		} else {
-			c.String(404, "Pair not found")
-		}
-	})
+	group.POST("/create", s.CreateCourse)
+	group.DELETE("/delete", s.DeleteCourse)
+	group.GET("/convert", s.ConvertCourse)
 }
